266c5220abda8fc4250d05a55975d9f86298c87d
Alex <alexbacirea@gmail.com>
2024-09-03 00:18:30 +0300

    Adds navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Navbar from '@/components/navbar';
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="flex justify-center w-full">
+      <Navbar />
+    </main>
+  );
 }
