cffbfb6eaadcda67c3e6caac776ab87f33ac8961
Alex <alexbacirea@gmail.com>
2024-09-17 02:28:18 +0300

    Adds minor style changes for service component.

--- components/services.tsx
+++ components/services.tsx
@@ -2,16 +2,17 @@ import Link from 'next/link';
 import { Button } from './ui/button';
 import { Info } from 'lucide-react';
 import ServicesList from './services-list';
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline';
 
 export default function Services() {
   return (
     <section className="flex flex-col gap-12 max-w-[1080px] w-full px-5">
       <div className="flex flex-row justify-between px-8">
-        <h1 className="scroll-m-20 text-4xl font-semibold tracking-tighter lg:text-5xl">
+        <h1 className="text-4xl font-semibold tracking-tighter lg:text-5xl">
           We&apos;d love to welcome <br></br>{' '}
           <span className="text-zinc-500">you to our practice.</span>
         </h1>
-        <Button className="self-end text-base rounded-full shadow-xl py-7 px-7 bg-zinc-800">
+        <Button className="self-end text-base rounded-full p-7 bg-zinc-50 border text-black hover:bg-white transition-colors">
           Book a call
         </Button>
       </div>
@@ -21,10 +22,11 @@ export default function Services() {
         <p className="text-zinc-500">
           You don&apos;t find any service?{' '}
           <Link
-            className="inline-flex items-center gap-1 underline text-black hover:text-zinc-500"
+            className="inline-flex items-center gap-1 underline text-black hover:text-orange-500 transition-colors"
             href="/services"
           >
             View all services
+            <ArrowTopRightOnSquareIcon className="h-4 w-4" />
           </Link>
         </p>
       </div>
