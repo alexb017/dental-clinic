76ebeb6614935c49f9f300d6f40dfbbe9d330777
Alex <alexbacirea@gmail.com>
2024-09-25 14:24:57 +0300

    Adds form functionality for appointments.

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
+import { set, useForm } from 'react-hook-form';
 import { z } from 'zod';
-
+import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import {
   Form,
@@ -15,6 +15,8 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import emailjs from '@emailjs/browser';
 
 const contactFormSchema = z.object({
   name: z
@@ -25,6 +27,7 @@ const contactFormSchema = z.object({
     .max(50, {
       message: 'Name must be less than 50 characters long',
     }),
+  email: z.string().email({ message: 'Invalid email address' }),
   phone: z.string().regex(/^\d{10}$/, {
     message: 'Phone number must be 10 digits long',
   }),
@@ -39,28 +42,63 @@ const contactFormSchema = z.object({
 });
 
 export default function ContactForm() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isMessageSent, setIsMessageSent] = useState<string | null>(null);
   const form = useForm<z.infer<typeof contactFormSchema>>({
     resolver: zodResolver(contactFormSchema),
     defaultValues: {
-      name: 'alex',
-      phone: '1234567890',
-      message: 'Hello World',
+      name: '',
+      email: '',
+      phone: '',
+      message: '',
     },
   });
 
+  // Submit form data to emailjs service
+  // https://www.emailjs.com/docs/examples/reactjs/
   function onSubmit(data: z.infer<typeof contactFormSchema>) {
-    console.log(data);
+    setIsSubmitting(true);
+    setIsMessageSent(null); // Reset message sent state
+
+    emailjs
+      .send(
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
+        data,
+        process.env.NEXT_PUBLIC_EMAILJS_USER_ID!
+      )
+      .then(
+        (result) => {
+          setIsMessageSent('Message sent successfully!');
+          form.reset();
+          setIsSubmitting(false);
+
+          // Reset message sent state after 2 seconds
+          setTimeout(() => {
+            setIsMessageSent(null);
+          }, 2000);
+        },
+        (error) => {
+          setIsMessageSent('Message failed to send. Please try again.');
+          setIsSubmitting(false);
+
+          // Reset message sent state after 2 seconds
+          setTimeout(() => {
+            setIsMessageSent(null);
+          }, 2000);
+        }
+      );
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
         <FormField
           control={form.control}
           name="name"
           render={({ field }) => (
-            <FormItem>
-              <FormLabel>Your name</FormLabel>
+            <FormItem className="space-y-1.5">
+              <FormLabel>Name</FormLabel>
               <FormControl>
                 <Input placeholder="Your name" {...field} />
               </FormControl>
@@ -70,10 +108,23 @@ export default function ContactForm() {
         />
         <FormField
           control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem className="space-y-1.5">
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Your email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
           name="phone"
           render={({ field }) => (
-            <FormItem>
-              <FormLabel>Phone number</FormLabel>
+            <FormItem className="space-y-1.5">
+              <FormLabel>Phone</FormLabel>
               <FormControl>
                 <Input placeholder="Phone number" {...field} />
               </FormControl>
@@ -85,16 +136,23 @@ export default function ContactForm() {
           control={form.control}
           name="message"
           render={({ field }) => (
-            <FormItem>
+            <FormItem className="space-y-1.5">
               <FormLabel>Message</FormLabel>
               <FormControl>
-                <Input placeholder="Leave us a message..." {...field} />
+                <Textarea placeholder="Leave us a message." {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <div className="flex items-center gap-5">
+          <Button type="submit" className="rounded-full">
+            {isSubmitting ? 'Sending...' : 'Send message'}
+          </Button>
+          {isMessageSent && (
+            <p className="text-sm text-muted-foreground">{isMessageSent}</p>
+          )}
+        </div>
       </form>
     </Form>
   );
