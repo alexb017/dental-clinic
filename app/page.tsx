08e4286b19de06243b4d2c608af3cdc04cdc3a02
Alex <alexbacirea@gmail.com>
2024-09-29 17:24:29 +0300

    style(header): update image view and increase title font size

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
-import Navbar from '@/components/navbar';
 import Header from '@/components/header';
 import Services from '@/components/services';
 import Pricing from '@/components/pricing';
 import About from '@/components/about';
 import Reviews from '@/components/reviews';
 import Contact from '@/components/contact';
-import Footer from '@/components/footer';
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center gap-20 my-20 w-full overflow-hidden">
+    <main className="flex flex-col items-center justify-center gap-20 mt-14 mb-20 w-full overflow-hidden">
       <Header />
       <Services />
       <Pricing />
--- components/header.tsx
+++ components/header.tsx
@@ -1,21 +1,10 @@
 export default function Header() {
   return (
-    <header className="flex flex-col items-center justify-center w-full">
-      <div
-        className="flex justify-center items-end pb-8 bg-zinc-100 rounded-3xl"
-        style={{
-          width: 'calc(1280px - 40px)',
-          height: '500px',
-          backgroundImage: 'url(/images/2149164286.jpg)',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      ></div>
-      <div className="max-w-[1080px] pt-8 px-5">
-        <h1 className="text-4xl md:text-6xl text-orange-500 text-center font-extrabold tracking-tighter">
-          We understand the importance of dental care and oral hygiene.
-        </h1>
-      </div>
+    <header className="flex flex-col items-center justify-center gap-8 w-full max-w-[1280px] px-5">
+      <div className="flex justify-center items-end bg-zinc-100 rounded-3xl w-full h-[540px] bg-[url(/images/2149164286.jpg)] bg-cover bg-center"></div>
+      <h1 className="max-w-[1080px] text-5xl md:text-6xl xl:text-7xl text-orange-500 text-center font-extrabold tracking-tighter">
+        We understand the importance of dental care, and your smile.
+      </h1>
     </header>
   );
 }
